986acd0e60843bec26b01c190e83cd61b7573626
Vipm27 <86101819+Vipm27@users.noreply.github.com>
2023-05-26 22:17:45 +0300

    Graphs and Hover

--- src/app/pages/setting/Setting.jsx
+++ src/app/pages/setting/Setting.jsx
@@ -1,14 +1,121 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './setting.css'
+import { Graphs } from './Graphs';
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+} from 'chart.js';
+import { faker } from '@faker-js/faker';
+
 const Setting = () => {
+    ChartJS.register(
+        CategoryScale,
+        LinearScale,
+        PointElement,
+        LineElement,
+        Title,
+        Tooltip,
+        Filler,
+      );
+      const [options,setOptions] = useState(
+        {responsive: true,
+        plugins: {
+          title: {
+            display: true,
+          },
+        },
+      })
+      const labels = ["01.01","10.01" ,"20.01" ,"30.01" ,"01.02" ,"10.02" ,"20.02","30.02", ];
+      const [data, setData] = useState ({
+        labels,
+        datasets: [
+          {
+            label: 'Temperature',
+            data: labels.map(() => faker.datatype.number({ min: 0, max: 1000 })),
+            borderColor: 'rgba(255, 159, 159, 0.9)',
+            backgroundColor: 'rgba(255, 159, 159, 0.9)',
+          },
+        ],
+      })
+
+    const clickMan = (e) =>{
+        if(e.target.id==="blrb_1"){
+            setData({
+                labels,
+                datasets: [
+                  {
+                    label: 'Temperature',
+                    data: labels.map(() => faker.datatype.number({ min: 0, max: 100 })),
+                    borderColor: 'rgba(255, 159, 159, 0.9)',
+                    backgroundColor: 'rgba(255, 159, 159, 0.9)',
+                  },
+                ],
+              })
+            setOptions(
+                {responsive: true,
+                plugins: {
+                  title: {
+                    display: true,
+                  },
+                },
+              })
+        }
+        if(e.target.id==="blrb_2"){
+            setData({
+                labels,
+                datasets: [
+                  {
+                    label: 'Освещенность',
+                    data: labels.map(() => faker.datatype.number({ min: 0, max: 30 })),
+                    borderColor: 'rgba(255, 159, 159, 0.9)',
+                    backgroundColor: 'rgba(255, 159, 159, 0.9)',
+                  },
+                ],
+              })
+            setOptions(
+                {responsive: true,
+                plugins: {
+                  title: {
+                    display: true,
+                  },
+                },
+              })
+        }
+        if(e.target.id==="blrb_3"){
+            setData({
+                labels,
+                datasets: [
+                  {
+                    label: 'Влажность',
+                    data: labels.map(() => faker.datatype.number({ min: 0, max: 100 })),
+                    borderColor: 'rgba(255, 159, 159, 0.9)',
+                    backgroundColor: 'rgba(255, 159, 159, 0.9)',
+                  },
+                ],
+              })
+            setOptions(
+                {responsive: true,
+                plugins: {
+                  title: {
+                    display: true,
+                  },
+                },
+              })
+        }
+    }
     return (
         <div>
+
               <div className="header">
         <div className="name">Ольга</div>
         <img alt='' src="./media/arrow-left-circle.png"/>
     </div>
-
-
     <div className="top">
 
         <div className="top__left">
@@ -91,23 +198,25 @@ const Setting = () => {
             </div>
 
             <div className="bottom__left__buttons">
-                <div className="bottom__left__radio__button">
-                    <input id="blrb_1" type="radio" name="bottom__left__radio__button" value="1" checked/>
+                <div className="bottom__left__radio__button" >
+                    <input id="blrb_1" type="radio" name="bottom__left__radio__button" value="1" onClick={clickMan} />
                     <label for="blrb_1">Температура</label>
+                    
                 </div>
                  
-                <div className="bottom__left__radio__button">
-                    <input id="blrb_2" type="radio" name="bottom__left__radio__button" value="2"/>
+                <div className="bottom__left__radio__button" >
+                    <input id="blrb_2" type="radio" name="bottom__left__radio__button" value="2" onClick={clickMan}/>
                     <label for="blrb_2">Освещенность</label>
                 </div>
                  
-                <div className="bottom__left__radio__button">
-                    <input id="blrb_3" type="radio" name="bottom__left__radio__button" value="3"/>
+                <div className="bottom__left__radio__button" >
+                    <input id="blrb_3" type="radio" name="bottom__left__radio__button" value="3" onClick={clickMan}/>
                     <label for="blrb_3">Влажность</label>
                 </div>
             </div>
 
             <div className="graf">
+                <Graphs options={options} data={data}/>
                 <img alt='' className="graf_img" src="./media/graf.svg"/>
             </div>
 
@@ -120,7 +229,7 @@ const Setting = () => {
 
             <div className="bottom__right__radio__buttons">
                 <div className="bottom__right__radio__button">
-                    <input id="brrb_1" type="radio" name="bottom__right__radio__button" value="1" checked/>
+                    <input id="brrb_1" type="radio" name="bottom__right__radio__button" value="1" />
                     <label for="brrb_1">Шляпа</label>
                 </div>
                  
